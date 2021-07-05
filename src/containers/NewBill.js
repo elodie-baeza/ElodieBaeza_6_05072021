b8ece984b8fae5508dadf39fe897271ad31d9b3b
elodie baeza <elodie.baeza@gmail.com>
2021-07-05 19:41:52 +0200

    modal's picture display

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -17,7 +17,7 @@ export default class NewBill {
   }
   handleChangeFile = e => {
     const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
-    const filePath = e.target.value.split(/\\/g)
+    const filePath = e.target.files[0].name.split(/\\/g)
     const fileName = filePath[filePath.length-1]
     this.firestore
       .storage
