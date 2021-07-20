3693c2654474767531cc56a482400d8c302d8db5
elodie baeza <elodie.baeza@gmail.com>
2021-07-20 12:33:01 +0200

    Test POST new bill

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -3,6 +3,7 @@ import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
 import { localStorageMock } from "../__mocks__/localStorage.js"
 import { ROUTES } from "../constants/routes.js"
+import userEvent from "@testing-library/user-event"
 import firebase from "../__mocks__/firebase.js"
 
 //container/NewBill - code coverage 80% ****************************
@@ -98,30 +99,89 @@ describe("Given I am connected as an employee", () => {
 // test d'intégration POST new bill *********************************
 
 describe("Given I am a user connected as Employee", () => {
-  describe("When I navigate to Dashboard", () => {
-    test("Send new bill in mock API POST", async () => {
-      //  const getSpy = jest.spyOn(firebase, "get")
-      //  const bills = await firebase.post()
-      //  expect(getSpy).toHaveBeenCalledTimes(1)
-      //  expect(bills.data.length).toBe(4)
+  describe("When I open new bill form", () => {
+    test("Then I submit valid bill", () => {
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee',
+      }))
+
+      document.body.innerHTML = NewBillUI()
+
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+
+      const expenseType = screen.getByTestId('expense-type')
+      userEvent.selectOptions(expenseType, [ screen.getByText('Transports') ])
+      expect(expenseType.value).toBe('Transports')
+
+      const date = screen.getByTestId('datepicker')
+      userEvent.type(date, '2021-07-19')
+      expect(date.value).toBe('2021-07-19')
+
+      const amount = screen.getByTestId('amount')
+      userEvent.type(amount, '23')
+      expect(amount.value).toBe('23')
+
+      const pct = screen.getByTestId('pct')
+      userEvent.type(pct, '20')
+      expect(pct.value).toBe('20')
+
+      const justificatif = screen.getByTestId('file')
+      const file = new File(['hello'], 'hello.png', { type: 'image/png' })
+      userEvent.upload(justificatif, file)
+      expect(justificatif.files[0]).toStrictEqual(file)
+      expect(justificatif.files.item(0)).toStrictEqual(file)
+      expect(justificatif.files).toHaveLength(1)
+
+      const bill = new NewBill({ document, onNavigate, firestore: null, localStorage: window.localStorage })
+
+      const form = screen.getByTestId('form-new-bill')
+      const handleSubmit = jest.fn((e) => bill.handleSubmit(e))
+
+      form.addEventListener("submit", handleSubmit)
+      fireEvent.submit(form) 
+      expect(handleSubmit).toHaveBeenCalledTimes(1)
     })
-    test("fetches bills from an API and fails with 404 message error", async () => {
-      // firebase.get.mockImplementationOnce(() =>
-      //   Promise.reject(new Error("Erreur 404"))
-      // )
-      // const html = DashboardUI({ error: "Erreur 404" })
-      // document.body.innerHTML = html
-      // const message = await screen.getByText(/Erreur 404/)
-      // expect(message).toBeTruthy()
+    test("Fetches bill from mock API post", async() => {
+      const dataBill = {
+        email:  'toto@test',
+        type: 'Transports',
+        name:  '',
+        amount: 50,
+        date:  '2021-07-19',
+        vat: '',
+        pct: 20,
+        commentary: '',
+        fileUrl: '',
+        fileName: 'hello.png',
+        status: 'pending'
+      }
+      const postSpy = jest.spyOn(firebase, 'post')
+      const bill = await firebase.post()
+      expect(dataBill.email).toMatch(bill.data[0].email)
+      expect(postSpy).toHaveBeenCalledTimes(1)
+      expect(bill.data.length).toBe(1)
     })
-    test("fetches messages from an API and fails with 500 message error", async () => {
-      // firebase.get.mockImplementationOnce(() =>
-      //   Promise.reject(new Error("Erreur 500"))
-      // )
-      // const html = DashboardUI({ error: "Erreur 500" })
-      // document.body.innerHTML = html
-      // const message = await screen.getByText(/Erreur 500/)
-      // expect(message).toBeTruthy()
+  })
+  describe("When new bill is created", () => {
+    test("Then bills table is displayed ", () => {
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+
+      document.body.innerHTML = NewBillUI()
+
+      const bill = new NewBill({ document, onNavigate, firestore: null, localStorage: window.localStorage })
+
+      const form = screen.getByTestId('form-new-bill')
+      const handleSubmit = jest.fn((e) => bill.handleSubmit(e))
+
+      form.addEventListener("submit", handleSubmit)
+      fireEvent.submit(form) 
+
+      expect(screen.getAllByText('Mes notes de frais')).toBeTruthy()
     })
   })
 })
