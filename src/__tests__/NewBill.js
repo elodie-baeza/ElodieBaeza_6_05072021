92ee4dd6d3c8fe5667777b7605d46dbe9caf03f0
elodie baeza <elodie.baeza@gmail.com>
2021-07-21 17:17:02 +0200

    Update POST test

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -41,12 +41,11 @@ describe("Given I am connected as an employee", () => {
     })
 
     test("Then I choose a require file", () => {
+      document.body.innerHTML = NewBillUI()
+
       const onNavigate = (pathname) => {
         document.body.innerHTML = ROUTES({ pathname })
       }
-
-      const html = NewBillUI()
-      document.body.innerHTML = html
 
       const newBill = new NewBill({ 
         document, 
@@ -71,39 +70,11 @@ describe("Given I am connected as an employee", () => {
       expect(btnDisable.disabled).not.toBeTruthy()
     })
 
-    test("Then I choose a require file", () => {
-      document.body.innerHTML = NewBillUI()
-      
-      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-
-      const onNavigate = (pathname) => {
-        document.body.innerHTML = ROUTES({ pathname })
-      }
-      
-      window.localStorage.setItem('user', JSON.stringify({
-        email: 'cedric.hiely@billed.com'
-      }))
-
-      const bill = new NewBill({ document, onNavigate, firestore: null, localStorage: window.localStorage })
-
-      const form = screen.getByTestId('form-new-bill')
-      const handleSubmit = jest.fn((e) => bill.handleSubmit(e))
-
-      form.addEventListener("submit", handleSubmit)
-      fireEvent.submit(form) 
-      expect(handleSubmit).toHaveBeenCalled()
-    })
-  })
-})
-
-// test d'intégration POST new bill *********************************
-
-describe("Given I am a user connected as Employee", () => {
-  describe("When I open new bill form", () => {
     test("Then I submit valid bill", () => {
       Object.defineProperty(window, 'localStorage', { value: localStorageMock })
       window.localStorage.setItem('user', JSON.stringify({
         type: 'Employee',
+        email: 'toto@test'
       }))
 
       document.body.innerHTML = NewBillUI()
@@ -114,25 +85,19 @@ describe("Given I am a user connected as Employee", () => {
 
       const expenseType = screen.getByTestId('expense-type')
       userEvent.selectOptions(expenseType, [ screen.getByText('Transports') ])
-      expect(expenseType.value).toBe('Transports')
 
       const date = screen.getByTestId('datepicker')
       userEvent.type(date, '2021-07-19')
-      expect(date.value).toBe('2021-07-19')
 
       const amount = screen.getByTestId('amount')
       userEvent.type(amount, '23')
-      expect(amount.value).toBe('23')
 
       const pct = screen.getByTestId('pct')
       userEvent.type(pct, '20')
-      expect(pct.value).toBe('20')
 
       const justificatif = screen.getByTestId('file')
       const file = new File(['hello'], 'hello.png', { type: 'image/png' })
       userEvent.upload(justificatif, file)
-      expect(justificatif.files[0]).toStrictEqual(file)
-      expect(justificatif.files.item(0)).toStrictEqual(file)
       expect(justificatif.files).toHaveLength(1)
 
       const bill = new NewBill({ document, onNavigate, firestore: null, localStorage: window.localStorage })
@@ -144,7 +109,14 @@ describe("Given I am a user connected as Employee", () => {
       fireEvent.submit(form) 
       expect(handleSubmit).toHaveBeenCalledTimes(1)
     })
-    test("Fetches bill from mock API post", async() => {
+  })
+})
+
+// test d'intégration POST new bill *********************************
+
+describe("Given I am a user connected as Employee", () => {
+  describe("When I fill new bill form", () => {
+    test("Fetches bill ID from mock API post", async() => {
       const dataBill = {
         email:  'toto@test',
         type: 'Transports',
@@ -156,16 +128,26 @@ describe("Given I am a user connected as Employee", () => {
         commentary: '',
         fileUrl: '',
         fileName: 'hello.png',
-        status: 'pending'
       }
       const postSpy = jest.spyOn(firebase, 'post')
-      const bill = await firebase.post()
-      expect(dataBill.email).toMatch(bill.data[0].email)
+      const postBill = await firebase.post(dataBill)
+
+      //mock de la fonction POST exécuté 1 fois
       expect(postSpy).toHaveBeenCalledTimes(1)
-      expect(bill.data.length).toBe(1)
+      //mock de la fonction POST retourné avec succé
+      expect(postSpy).toReturn()
+      //l'ID est présent dans le POST
+      expect(postBill).toHaveProperty("id")
+      expect(postBill.id).toBeTruthy()
+      expect(postBill.id).toMatch("qcCK3SzECmaZAGRrHjaC")
+      //la bills posté est présente dans la table de bills 
+      const getSpy = jest.spyOn(firebase, "get")
+      const bills = await firebase.get()
+      expect(getSpy).toHaveBeenCalledTimes(1)
+      expect(bills.data[0].id).toBe(postBill.id)
     })
   })
-  describe("When new bill is created", () => {
+  describe("When new bill is submit", () => {
     test("Then bills table is displayed ", () => {
       const onNavigate = (pathname) => {
         document.body.innerHTML = ROUTES({ pathname })
